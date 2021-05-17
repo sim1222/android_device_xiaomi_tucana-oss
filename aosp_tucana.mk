21ca7b89432aa2cf7323ea7b15e4196c84896320
こけっち <50144466+sim1222@users.noreply.github.com>
2021-05-18 08:05:11 +0900

    Update aosp_tucana.mk

--- aosp_tucana.mk
+++ aosp_tucana.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, device/xiaomi/tucana/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_tucana
